63ac8bf80d7b990d58f4420acf3d6fd9b498449f
norbertozga <ozgn@viessmann.com>
2025-07-25 15:31:06 +0200

    Add default sub-reporter

---
+++ backend/analyser_cli/sub_reporters/__init__.py
@@ -0,0 +1,17 @@
+"""
+Sub-reporters module for SNP Analyser
+
+This module contains specialized reporting classes that generate focused analysis reports
+for different aspects of DNA sequence analysis and mutation effects.
+
+Each sub-reporter inherits from the SubReport base class and provides specific
+analysis capabilities for different types of genetic analysis.
+"""
+
+from .mutation_effects import MutationEffectsReport
+
+__all__ = [
+    'MutationEffectsReport'
+]
+
+__version__ = "0.1.0"
